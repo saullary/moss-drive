964ddd2026da7f656e57409cccd1ea390e7a49c4
hqs <imhuangqs@gmail.com>
2023-09-26 15:43:14 +0800

    mossy

---
+++ src/layouts/mossy-layout.vue
@@ -0,0 +1,75 @@
+<style lang="scss">
+body.body--dark {
+  background-image: url(/img/mossy/bg-top-1.png);
+  background-position: 100% 0;
+  background-repeat: no-repeat;
+  background-size: contain;
+}
+.main-wrap {
+  max-width: 1100px;
+  min-width: 750px;
+  margin: 0 auto;
+  padding: 0 20px;
+}
+.bg-active {
+  background: #0000006e;
+  color: #fff;
+}
+</style>
+
+<template>
+  <div class="pa-2"></div>
+  <div class="pos-s z-100 top-0">
+    <div class="main-wrap">
+      <div class="bdrs-100 al-c bg-primary">
+        <div class="al-c q-py-sm">
+          <img src="/img/moss.svg" height="28" class="ml-5" />
+          <img src="/img/mossy/mossyland.svg" height="28" class="ml-2" />
+        </div>
+        <div class="ml-5">
+          <q-btn
+            v-for="it in menus"
+            :key="it.path"
+            rounded
+            flat
+            :class="{
+              'bg-active': it.path == path,
+            }"
+            :to="'/mossyland' + it.path"
+            >{{ it.label }}</q-btn
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+  <router-view></router-view>
+  <p v-for="i in 40" :key="i">{{ i }}</p>
+</template>
+
+<script>
+export default {
+  computed: {
+    path() {
+      return this.$route.path.replace("/mossyland", "");
+    },
+  },
+  data() {
+    return {
+      menus: [
+        {
+          path: "/moments",
+          label: "Moments",
+        },
+        {
+          path: "/explore",
+          label: "Explore",
+        },
+        {
+          path: "/collected",
+          label: "Collected",
+        },
+      ],
+    };
+  },
+};
+</script>
--- src/pages/routes.js
+++ src/pages/routes.js
@@ -1,32 +1,49 @@
 const routes = [
   {
-    path: '/',
-    component: () => import('../layouts/main-layout.vue'),
+    path: "/",
+    component: () => import("../layouts/main-layout.vue"),
     children: [
-      { path: '', component: () => import('./home-page.vue') },
+      { path: "", component: () => import("./home-page.vue") },
       {
-        path: '/drive/:catchAll(.*)*',
-        component: () => import('./drive/page-drive.vue'),
+        path: "/drive/:catchAll(.*)*",
+        component: () => import("./drive/page-drive.vue"),
         meta: {
-          title: 'My Drive'
-        }
+          title: "My Drive",
+        },
       },
       {
-        path: '/stone',
-        component: () => import('./stone/page-stone.vue'),
+        path: "/stone",
+        component: () => import("./stone/page-stone.vue"),
         meta: {
-          title: 'My Stones'
-        }
-      }
-    ]
+          title: "My Stones",
+        },
+      },
+    ],
   },
-
+  {
+    path: "/mossyland",
+    component: () => import("../layouts/mossy-layout.vue"),
+    children: [
+      {
+        path: "moments",
+        component: () => import("./mossy/page-moments.vue"),
+      },
+      {
+        path: "explore",
+        component: () => import("./mossy/page-explore.vue"),
+      },
+      {
+        path: "collected",
+        component: () => import("./mossy/page-collected.vue"),
+      },
+    ],
+  },
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('./ErrorNotFound.vue')
-  }
-]
+    path: "/:catchAll(.*)*",
+    component: () => import("./ErrorNotFound.vue"),
+  },
+];
 
-export default routes
+export default routes;
