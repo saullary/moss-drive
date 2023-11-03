b43f00322a910fb419ec0670bcacdae5727638c3
hqs <imhuangqs@gmail.com>
2023-11-03 16:12:52 +0800

    deploy

--- src/pages/resource/page-resource.vue
+++ src/pages/resource/page-resource.vue
@@ -27,34 +27,5 @@
         />
       </div>
     </div>
-
-    <div class="bg-card-1 mt-4 ov-h">
-      <div class="pa-4">
-        <div class="title">Purchase</div>
-      </div>
-      <div class="pa-3"></div>
-      <div class="pa-4 al-c" style="background: #94f9ca; color: #333">
-        <div class="fz-13 mr-auto">
-          <p>Storage: <b class="fz-14">1GB/20GB</b></p>
-          <p class="mt-1">Expiration date: <b class="fz-14">Apr 2 2024</b></p>
-        </div>
-        <div class="ta-r mr-3">
-          <p>Total Costs</p>
-          <p>
-            <b class="fz-20 mr-1">30.22</b>
-            <span class="fz-12">USDC</span>
-          </p>
-        </div>
-        <div>
-          <q-btn rounded color="white" unelevated>
-            <span class="gray-3">Approve</span>
-          </q-btn>
-        </div>
-      </div>
-    </div>
-
-    <div class="bg-card-1 mt-4 pa-4">
-      <div class="title">Bill Details</div>
-    </div>
   </div>
 </template>
